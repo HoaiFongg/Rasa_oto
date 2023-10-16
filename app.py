e02cb551d3abb279c17a78666488911145f7df6d
HoaiFongg <hoaifongg@gmail.com>
2023-10-17 00:10:45 +0700

    new-17-10-2023

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from  flask import Flask, render_template
+from  flask import Flask, render_template, request, jsonify
+import requests
 
-
+RASA_API_URL = 'http://localhost:5005/webhooks/rest/webhook'
 
 app = Flask(__name__)
 
@@ -8,6 +9,23 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/webhook',methods=['POST'])
+def webhook():
+    user_message = request.json['message']
+    print("User message: " + user_message)
+
+    #send user message to Rasa and get bot's reponse
+    rasa_response =requests.post(RASA_API_URL, json = {'message':user_message})
+    rasa_response_json = rasa_response.json()
+
+    print("Rasa reponse: ", rasa_response_json)
+    
+    bot_response = rasa_response_json[0]['text'] if rasa_response_json else 'sorry, i didn\'t understand that.'
+    return jsonify({"response": bot_response})
+
+
 
 
 if __name__ == '__main__':
